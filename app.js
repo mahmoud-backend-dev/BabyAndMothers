cf08216604e602cec618c26309e54174bd487846
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-04-09 15:29:23 +0200

    rateLimiter

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(mongoSanitize());
 app.use(xss())
 
 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-const limiter = rateLimit({
+const limiter = reteLimiter({
 	windowMs: 15 * 60 * 1000,  // 15 minutes
     max: 100, 
     message:
