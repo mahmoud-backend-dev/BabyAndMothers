a1b017800631b317a8612dddfa5a8e61f1bc4f02
Mohamed Ayman <mohamedasmr98@gmail.com>
2023-04-09 17:39:07 +0200

    Init Commit

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
 const port = 1812 | process.env.PORT;
-const session = require('express-session');
-const authRoute = require('./routes/authRoute');
-const connectDB = require('./db/connectDB');
-const { configPassport } = require('./utils/passport');
-const passport = require('passport');
-const errorHandler = require('./middleware/error-handler');
-const notFoundError = require('./middleware/notFoundMiddleware');
-const compression = require('compression');
-const cors = require('cors');
-const reteLimiter = require('express-rate-limit');
-const hpp = require('hpp');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const MongoStore = require('connect-mongo');
+const session = require("express-session");
+const authRoute = require("./routes/authRoute");
+const connectDB = require("./db/connectDB");
+const { configPassport } = require("./utils/passport");
+const passport = require("passport");
+const errorHandler = require("./middleware/error-handler");
+const notFoundError = require("./middleware/notFoundMiddleware");
+const compression = require("compression");
+const cors = require("cors");
+const reteLimiter = require("express-rate-limit");
+const hpp = require("hpp");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const MongoStore = require("connect-mongo");
+const weeksRouter = require("./routes/weeks");
 
 configPassport();
 
 // configure session middleware
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: false,
-  store: MongoStore.create({ mongoUrl: process.env.URI })
-}));
+app.use(
+    session({
+        secret: "your-secret-key",
+        resave: false,
+        saveUninitialized: false,
+        store: MongoStore.create({ mongoUrl: process.env.URI }),
+    })
+);
 
 // Enable other domains to access your application
 app.use(cors());
-app.options('*', cors());
-
+app.options("*", cors());
 
 // Compress all responses
 app.use(compression());
@@ -41,40 +43,41 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-
 // To remove data using these defaults, To apply data sanitization
 // nosql mongo injection
 app.use(mongoSanitize());
 // To sanitize user input coming from POST body, GET queries, and url params  ex: '<script></script>' to convert string ''&lt;script>&lt;/script>''
-app.use(xss())
+app.use(xss());
 
 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 const limiter = reteLimiter({
-	windowMs: 15 * 60 * 1000,  // 15 minutes
-    max: 100, 
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
     message:
-    'Too many accounts created from this IP, please try again after an 15 minutes'
-})
+        "Too many accounts created from this IP, please try again after an 15 minutes",
+});
 
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 
 // Express middleware to protect against HTTP Parameter Pollution attacks
 app.use(hpp());
 
-
-app.use('/api/v1/auth', authRoute);
+app.use("/api/v1/auth", authRoute);
 app.use(errorHandler);
 app.use(notFoundError);
 
+app.use(weeksRouter);
+
 const start = async () => {
-  try {
-    await connectDB(process.env.URI);
-    app.listen(port, () => console.log(`Listen on http://localhost:${port}`));
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.URI);
+        app.listen(port, () =>
+            console.log(`Listen on http://localhost:${port}`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
-start()
+start();
---
+++ routes/weeks.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = new express.Router();
+const weeksController = require("../controller/weeks-Controller");
+const cors = require("cors");
+
+router.use(
+    cors({
+        origin: "*",
+    })
+);
+
+router.post("/add/week", weeksController.addWeek);
+
+router.get("/search/:key", weeksController.readWeek);
+
+router.patch("/update/:id", weeksController.updateWeek);
+
+router.delete("/delete/:id", weeksController.deleteWeek);
+
+module.exports = router;
