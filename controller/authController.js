955847df26875d374530d4a7266ad2c06174fcee
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-04-14 15:54:07 +0200

    update controller

--- controller/authController.js
+++ controller/authController.js
@@ -60,13 +60,78 @@ exports.varifyResetCodeForSignup = asyncHandler(async (req, res, next) => {
     resetCodeExpiredForSignup: { $gt: Date.now() },
   })
   if (!user)
-    throw new BadRequest('Reset code invalid or expired')
+    throw new BadRequest(`Reset code invalid or expired or no email for this: ${req.body.email} `)
   
   user.hashedResetCodeForSignup = undefined;
   user.resetCodeExpiredForSignup = undefined;
   user.resetVerifyForSignup = true;
   const token = user.createJWT()
   res.status(StatusCodes.OK).json({ message: "Success", token, data: santizeData(user) });
+});
+
+exports.resendRestCodeForSignup = asyncHandler(async (req, res) => {
+  // Get User by email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    throw new NotFoundError(`There is no user with that email ${req.body.email}`);
+  
+  // Generate reset code (6-digit)
+  const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
+  const hashedResetCode = hashedResetCodeByCrypto(resetCode);
+  user.hashedResetCodeForSignup = hashedResetCode;
+  user.resetCodeExpiredForSignup = Date.now() + 10 * 60 * 1000;
+  user.resetVerifyForSignup = false;
+  await user.save();
+  const mailOpts = {
+    from: `Mahmod Hamdi <mh15721812@gmail.com>`,
+    to: user.email,
+    subject: `Your reset code for register our app (valid for 10 min)`,
+    text: `Hi ${user.firstName}
+    G-${resetCode}  is your varification code for register our app ( Babies And Mothers Application)`
+  };
+  try {
+    await sendEmail(mailOpts);
+  } catch (error) {
+    user.hashedResetCodeForSignup = undefined;
+    user.resetCodeExpiredForSignup = undefined;
+    user.resetVerifyForSignup = true;
+    await user.save();
+    throw new CustomErrorAPI('There is an error in sending email', StatusCodes.INTERNAL_SERVER_ERROR);
+  };
+  res.status(StatusCodes.OK).json({ status: 'Success', message: 'Reset code sent to email' })
+});
+
+
+exports.resendRestCodeForPassword = asyncHandler(async (req, res) => {
+  // Get User by email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user)
+    throw new NotFoundError(`There is no user with that email ${req.body.email}`);
+  
+  // Generate reset code (6-digit)
+  const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
+  const hashedResetCode = hashedResetCodeByCrypto(resetCode);
+  user.hashedResetCodeForPassword = hashedResetCode;
+  user.resetCodeExpiredForPassword = Date.now() + 10 * 60 * 1000;
+  user.resetVerifyForPassword = false;
+  await user.save();
+  const mailOpts = {
+    from: `Mahmoud Hamdi <mh15721812@gmail.co>`,
+    to: user.email,
+    subject: 'Reset code for forget password to change your password',
+    text: `Hi ${user.firstName}
+    G-${resetCode}  is your varification code for change your Password ( Babies And Mothers Application)`
+  };
+  try {
+    await sendEmail(mailOpts);
+  } catch (error) {
+    user.hashedResetCodeForPassword = undefined;
+    user.resetCodeExpiredForPassword = undefined;
+    user.resetVerifyForPassword = undefined;
+    await user.save();
+    throw new CustomErrorAPI('There is an error in sending email', StatusCodes.INTERNAL_SERVER_ERROR);
+  };
+  res.status(StatusCodes.OK).json({ status: 'Success', message: 'Reset code sent to email' })
 });
 
 exports.login = asyncHandler(async (req, res, next) => {
@@ -126,7 +191,7 @@ exports.varifyResetCodeForPassword = asyncHandler(async (req, res) => {
   });
 
   if (!user)
-    throw new BadRequest('Reset code invalid or expired')
+    throw new BadRequest(`Reset code invalid or expired or no email for this: ${req.body.email} `)
   user.resetVerifyForPassword = true;
   await user.save();
   res.status(StatusCodes.OK).json({ status: "Success" });
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,9 @@ const {
   login,
   varifyResetCodeForPassword,
   forgetPassword,
-  resetPassword
+  resetPassword,
+  resendRestCodeForSignup,
+  resendRestCodeForPassword
 } = require('../controller/authController')
 const {
   signupValidator,
@@ -31,12 +33,14 @@ router.get('/google',
     });
   
 router.post('/signup', signupValidator, signup);
+router.post('/signup/resendResetCode', forgetPasswordValidator, resendRestCodeForSignup);
 
 router.post('/login', loginValidator ,login);
 
 router.post('/varifyResetCodeForSignup', varifyCodeValidator, varifyResetCodeForSignup);
 
 router.post('/forgetPassword', forgetPasswordValidator, forgetPassword);
+router.post('/forgetPassword/resendResetCode',forgetPasswordValidator, resendRestCodeForPassword);
 router.post('/varifyResetCodeForPassword', varifyCodeValidator, varifyResetCodeForPassword);
 router.post('/resetPassword',resetPasswordValidator,resetPassword)
 
