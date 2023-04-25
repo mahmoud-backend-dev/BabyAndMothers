a92f9aba2562fa00c1f07625bf3a467c409b8111
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2023-04-25 17:57:28 +0200

    Update validatorMiddleware.js

--- middleware/validatorMiddleware.js
+++ middleware/validatorMiddleware.js
@@ -1,10 +1,15 @@
+const fs = require('fs');
 const { validationResult } = require('express-validator');
 
 // Finds the validation errors in this request and wraps them in an object with handy functions
-const validatorMiddleWare = (req, res,next) => {
+const validatorMiddleWare = (req, res, next) => {
     const errors = validationResult(req);
-    if (!errors.isEmpty())
+    if (!errors.isEmpty()) {
+        if (req.file) {
+            fs.unlinkSync(req.file.path);
+        };
         return res.status(400).json({ errors: errors.array() });
+    }
     next();
 }
 
